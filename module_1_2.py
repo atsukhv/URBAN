0ae3e1d140bdce080dd5f0f3c1400187a7c33db1
Albert <atsukhv@gmail.com>
2024-08-06 10:18:55 +0300

    Add files via upload

---
+++ module_1_2.py
@@ -0,0 +1,7 @@
+completed_dz = 12
+wasted_time = 1.5
+course_name = "Python"
+
+
+print('Курс: ', course_name, 'Всего задач: ', completed_dz, 'Затрачено часов: ', wasted_time,
+      'Среднее время выполнения: ', completed_dz / wasted_time)
---
+++ module_1_3.py
@@ -0,0 +1,11 @@
+name = 'Albert'
+print(name)
+
+age = 20
+print(age)
+
+age = age + 2
+print(age)
+
+is_student = True
+print(is_student)
