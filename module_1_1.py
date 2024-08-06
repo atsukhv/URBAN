c1e00e148cfcf87d807deaafef7abe556c116f79
Albert <atsukhv@gmail.com>
2024-08-06 12:06:42 +0300

    Update module_1_1.py

--- module_1_1.py
+++ module_1_1.py
@@ -4,4 +4,4 @@ print(example[0])  # первый символ
 print(example[-1])  # последний символ
 print(example[3:])  # вторая половина
 print(example[::-1])  # обратная строка
-
+print(example[1::2])  # каждый второй символ
