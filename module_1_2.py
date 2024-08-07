e14b33c37406c9e0fd90db21ea10859367751a3a
Albert <atsukhv@gmail.com>
2024-08-07 12:17:52 +0300

    Update module_1_2.py

--- module_1_2.py
+++ module_1_2.py
@@ -4,4 +4,4 @@ course_name = "Python"
 
 
 print('Курс: ', course_name, 'Всего задач: ', completed_dz, 'Затрачено часов: ', wasted_time,
-      'Среднее время выполнения: ', completed_dz / wasted_time)
+      'Среднее время выполнения: ', wasted_time / completed_dz)
