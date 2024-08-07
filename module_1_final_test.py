7ad46de9f702684c2c08e63df3ff102775b5db20
Albert <atsukhv@gmail.com>
2024-08-07 08:59:49 +0300

    Add files via upload

---
+++ module_1_final_test.py
@@ -0,0 +1,14 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+results = []  # список с результатами
+
+students = list(students)  # преобразование множества в список
+students.sort()  # сортировка по алфавиту
+
+for sublist in grades:
+    quantity = len(sublist)  # вычисляем количество оценок в подсписке
+    summ = sum(sublist)  # вычисляем сумму оценок
+    result = summ / quantity  # вычисляем средний балл
+    results.append(result)  # добавляем средний балл в список
+
+print(dict(zip(students, results)))  # объединяем списки в словарь и выводим значение в консоль
