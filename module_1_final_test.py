d94845c15c5ae808194086adf8191dbdfb875d91
Albert <atsukhv@gmail.com>
2024-08-07 09:01:27 +0300

    Update module_1_final_test.py

--- module_1_final_test.py
+++ module_1_final_test.py
@@ -1,6 +1,6 @@
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
-results = []  # список с результатами
+results = []  # список с средними баллами
 
 students = list(students)  # преобразование множества в список
 students.sort()  # сортировка по алфавиту
