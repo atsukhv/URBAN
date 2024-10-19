deb2342484502c52e9acfc23a3754c5be8cdbf58
Albert <atsukhv@gmail.com>
2024-10-19 18:44:48 +0300

    Update module_13_2.py

--- module_13/module_13_2.py
+++ module_13/module_13_2.py
@@ -13,12 +13,10 @@ dp = Dispatcher(bot, storage=storage)
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
     print('Привет! Я бот помогающий твоему здоровью.')
-    await message.reply('Привет! Я бот помогающий твоему здоровью.')
 
 @dp.message_handler()
 async def all_messages(message: types.Message):
     print('Введите команду /start, чтобы начать общение.')
-    await message.reply('Введите команду /start, чтобы начать общение.')
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
