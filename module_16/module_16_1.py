374de03de6a87fc3f4d9cf0215c7ca5aef7a7af0
rocket <atsukhv@gmail.com>
2025-01-14 12:08:26 +0300

    m16

--- module_16/module_16_1.py
+++ module_16/module_16_1.py
@@ -5,19 +5,19 @@ app = FastAPI()
 
 @app.get("/")
 async def read_root():
-    return {"message": "Главная страница"}
+    return "Главная страница"
 
 
 @app.get("/user/admin")
 async def read_admin():
-    return {"message": "Вы вошли как администратор"}
+    return "Вы вошли как администратор"
 
 
 @app.get("/user/{user_id}")
 async def read_user(user_id: int):
-    return {"message": f"Вы вошли как пользователь № {user_id}"}
+    return f"Вы вошли как пользователь № {user_id}"
 
 
 @app.get("/user")
 async def read_user(username: str, age: int):
-    return {"message": f"Информация о пользователе. Имя: {username}, Возраст: {age}"}
+    return f"Информация о пользователе. Имя: {username}, Возраст: {age}"
